494ea2fb590bf80b1c2c833ac983c6eb1b6b239f
Sebastian Zasuwa <sebastianzasuwa@gmail.com>
2019-03-15 23:39:21 +0100

    Added player movement and gravity (need to adjust forces and smooth movement)

--- Project PO/Player.cpp
+++ Project PO/Player.cpp
@@ -3,6 +3,8 @@
 Player::Player(const sf::Vector2u windowSize) : F_WINDOW_SIZE(windowSize)
 {
 	fDrawable = new sf::RectangleShape(sf::Vector2f(10, 10));
+	fPositionVector.x = 0;
+	fPositionVector.y = 0;
 }
 
 Player::~Player()
@@ -11,5 +13,53 @@ Player::~Player()
 
 void Player::update() 
 {
+	controlMovement();
+	controlGravity();
+	applyMovement();
+}
 
+void Player::controlGravity()
+{
+	fMovementVector.y += fGravityAcceleration;
+	fMovementVector.y = std::min(fMovementVector.y, fGravityForce);
+}
+
+void Player::hitGround() 
+{
+	fIsJumping = false;
+}
+
+void Player::controlMovement()
+{
+	//Reset movement vector
+	fMovementVector.x = 0;
+
+	//Check movement keys
+	if (sf::Keyboard::isKeyPressed(sf::Keyboard::A))
+	{
+		fMovementVector.x -= fSpeed;
+	}
+	if (sf::Keyboard::isKeyPressed(sf::Keyboard::D))
+	{
+		fMovementVector.x += fSpeed;
+	}
+	if (sf::Keyboard::isKeyPressed(sf::Keyboard::Space) && !fIsJumping)
+	{
+		fIsJumping = true;
+		fMovementVector.y = fJumpForce;
+	}
+}
+
+void Player::applyMovement() 
+{
+	//Set position vector
+	fPositionVector.x += fMovementVector.x;
+	fPositionVector.y += fMovementVector.y;
+
+	//Prevent escaping window boundaries
+	fPositionVector.x = std::max(0.0f, std::min(fPositionVector.x, F_WINDOW_SIZE.x - ((sf::RectangleShape*)fDrawable)->getSize().x));
+	fPositionVector.y = std::max(0.0f, std::min(fPositionVector.y, F_WINDOW_SIZE.y - ((sf::RectangleShape*)fDrawable)->getSize().y));
+
+	//Set position
+	((sf::RectangleShape*)fDrawable)->setPosition(fPositionVector);
 }
--- Project PO/Player.h
+++ Project PO/Player.h
@@ -1,10 +1,24 @@
 #pragma once
 #include "GameObject.h"
 #include <SFML/Graphics.hpp>
+#include <algorithm>
 
 class Player : public GameObject
 {
 	const sf::Vector2u F_WINDOW_SIZE;
+	//Movement
+	sf::Vector2f fPositionVector;
+	sf::Vector2f fMovementVector;
+	int fSpeed = 1;
+	float fGravityAcceleration = 0.05f;
+	float fGravityForce = 5.0f;
+	bool fIsJumping = false;
+	float fJumpForce = -2.0f;
+
+	void controlMovement();
+	void controlGravity();
+	void applyMovement();
+	void hitGround();
 
 public:
 	Player(const sf::Vector2u windowSize);

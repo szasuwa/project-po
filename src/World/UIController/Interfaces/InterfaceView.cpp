8d6825b83af9dfb32d1178bc14aa4ca0b28429bf
Sebastian Zasuwa <sebastianzasuwa@gmail.com>
2019-05-08 21:06:22 +0200

    Added properies to views

--- src/World/UIController/Interfaces/InterfaceView.cpp
+++ src/World/UIController/Interfaces/InterfaceView.cpp
@@ -29,3 +29,19 @@ void InterfaceView::draw(UIInterface& f)
 		}
 	}
 }
+
+void InterfaceView::setProperty(const unsigned int& i, const std::string& v)
+{
+	if (i >= fPropertyList.size())
+		throw std::out_of_range("Property does not exist (Size: " + std::to_string(fPropertyList.size()) + " , requested: " + std::to_string(i) + ")");
+
+	fPropertyList[i] = v;
+}
+
+std::string InterfaceView::getProperty(const unsigned int& i)
+{
+	if (i >= fPropertyList.size())
+		throw std::out_of_range("Property does not exist (Size: " + std::to_string(fPropertyList.size()) + " , requested: " + std::to_string(i) + ")");
+
+	return fPropertyList[i];
+}
--- src/World/UIController/Interfaces/InterfaceView.h
+++ src/World/UIController/Interfaces/InterfaceView.h
@@ -4,14 +4,17 @@
 #include "../../Interfaces/UIViewInterface.h"
 #include "ViewGroup.h"
 
-
 class InterfaceView : public UIViewInterface {
 protected:
 	std::vector<ViewGroup*> fGroupList;
+	std::vector<std::string> fPropertyList;
 
 public:
 	virtual ~InterfaceView() {}
 	virtual void updateView(UIInterface& f);
 	virtual void update(UIInterface& f);
 	virtual void draw(UIInterface& f);
+	
+	void setProperty(const unsigned int& i, const std::string& v);
+	std::string getProperty(const unsigned int& i);
 };
